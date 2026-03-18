268156bb4feecc6c4a42a10ee20aed67e1429362
杨兵 <859696354@qq.com>
2026-03-18 22:13:50 +0800

    fix: tighten tauri shell baseline

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/app.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('Missing root element')
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
