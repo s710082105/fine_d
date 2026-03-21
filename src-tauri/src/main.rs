5c4632985e52a0a2c5c02082c731281f64f72019
杨兵 <859696354@qq.com>
2026-03-21 16:38:30 +0800

    fix: Windows 启动时隐藏控制台窗口
    添加 windows_subsystem = "windows" 属性，Release 模式下不再弹出
    命令行窗口。Debug 模式保留控制台便于调试。

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,6 @@
+// Release 模式下隐藏 Windows 控制台窗口
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
     match finereport_tauri_shell_lib::commands::project_sync::try_run_cli() {
         Ok(true) => return,
